3d19cd448abe5d1f7197db2fbcc9d8f957f63935
Valdir Silva <valdir.ti@gmail.com>
2023-05-19 18:20:11 -0300

    feat: add icon new conversation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import IconClose from "./icons/IconClose"
+import IconAdd from "./icons/IconAdd"
 
 type SidebarProps = {
     open: boolean
@@ -16,7 +17,16 @@ export const Sidebar = ({ open, onClose, children}: SidebarProps) => {
 
         <div className={`transition-all duration-300 flex h-screen ${open ? "ml-0" : "-ml-96"} md:ml-0`}>
             <div className="flex flex-col w-64 p-2 bg-gray-900">
-                barra
+
+               <div className="flex items-center p-3 rounded-md text-sm cursor-pointer border border-white/20 hover:bg-gray-500/20">
+                    <IconAdd height={16} width={16} className="mr-3"/>
+                    Nova Conversa
+               </div>
+
+               <div></div>
+
+               <div></div>
+
             </div>
             <div className="flex justify-center items-center w-10 h-10 cursor-pointer md:hidden" onClick={onClose}>
                 <IconClose width={24} height={24}/>
